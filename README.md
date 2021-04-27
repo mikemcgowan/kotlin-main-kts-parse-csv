a032be81ce5699622981374bf744f8f6c843a7f5
Mike McGowan <mike.mcgowan@peoplefluent.com>
2021-04-27 14:55:25 +0100

    Added http (using Fuel)

--- README.md
+++ README.md
@@ -1,25 +1,10 @@
 # Parse CSV
 
-Known to work with Kotlin 1.4.31 (but should work from Kotlin [1.3.70](https://blog.jetbrains.com/kotlin/2020/03/kotlin-1-3-70-released/#scripting)).
+Known to work with Kotlin 1.4.31 (but should work from
+Kotlin [1.3.70](https://blog.jetbrains.com/kotlin/2020/03/kotlin-1-3-70-released/#scripting)).
 
-Uses [kotlin-csv](https://github.com/doyaaaaaken/kotlin-csv).
+Top level dependencies:
 
-```
-$ sdk current kotlin
-Using kotlin version 1.4.31
-
-$ cat users.csv
-"employeeId","userName","firstName","lastName","email","language","group1","group2","favourite.colour","lucky.number"
-"10001","marty.byrde","Marty","Byrde","marty.byrde@peoplefluent.com","en","Red","Blue","red","7"
-"10002","wendy.byrde","Wendy","Byrde","wendy.byrde@peoplefluent.com","en","Red","","blue",""
-
-$ ./parse-csv.main.kts
-Missing input CSV file as first argument
-
-$ ./parse-csv.main.kts does-not-exist.csv
-CSV file "does-not-exist.csv" does not exist
-
-$ ./parse-csv.main.kts users.csv
-Username is "marty.byrde", first name is "Marty", last name is "Byrde", email is "marty.byrde@peoplefluent.com"
-Username is "wendy.byrde", first name is "Wendy", last name is "Byrde", email is "wendy.byrde@peoplefluent.com"
-```
+* [kotlin-csv](https://github.com/doyaaaaaken/kotlin-csv)
+* [fuel](https://github.com/kittinunf/Fuel)
+* [fuel-json](https://github.com/kittinunf/fuel/tree/master/fuel-json)
--- parse-csv.main.kts
+++ parse-csv.main.kts
@@ -5,12 +5,31 @@
 @file:DependsOn("org.slf4j:slf4j-simple:1.7.30")
 @file:DependsOn("org.slf4j:slf4j-api:1.7.30")
 @file:DependsOn("io.github.microutils:kotlin-logging:1.7.9")
+@file:DependsOn("org.json:json:20210307")
 @file:DependsOn("com.github.doyaaaaaken:kotlin-csv-jvm:0.15.2")
+@file:DependsOn("com.github.kittinunf.fuel:fuel:2.3.1")
+@file:DependsOn("com.github.kittinunf.fuel:fuel-json:2.3.1")
 
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import com.github.kittinunf.fuel.core.Headers
+import com.github.kittinunf.fuel.core.extensions.jsonBody
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.fuel.json.responseJson
+import org.json.JSONObject
 import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.system.exitProcess
+
+fun die(reason: String) {
+    println(reason)
+    exitProcess(1)
+}
+
+fun getenv(name: String): String {
+    val v = System.getenv(name)
+    if (v == null) die("Missing env var $name")
+    return v
+}
 
 if (args.isEmpty()) {
     die("Missing input CSV file as first argument")
@@ -21,22 +40,32 @@ if (!Files.exists(Paths.get(inputFile))) {
     die("""CSV file "$inputFile" does not exist""")
 }
 
+var url = getenv("URL")
+var clientId = getenv("CLIENT_ID")
+var clientSecret = getenv("CLIENT_SECRET")
+val credentialsUrl = "$url/auth/oauth/v2/token?grant_type=client_credentials&client_id=$clientId&client_secret=$clientSecret"
+
+val (_, _, result) = credentialsUrl.httpPost().responseJson()
+if (result.component2() != null) {
+    die("Couldn't obtain an iPaaS API access token")
+}
+val accessToken = result.component1()?.obj()?.get("access_token").toString()
+
 csvReader().open(inputFile) {
     readAllWithHeaderAsSequence().forEach { processRow(it) }
 }
 
-fun die(reason: String) {
-    println(reason)
-    exitProcess(1)
-}
-
-/**
- * TODO implement this function
- */
 fun processRow(row: Map<String, String>) {
-    val username = row["userName"]
-    val firstName = row["firstName"]
-    val lastName = row["lastName"]
-    val email = row["email"]
-    println("""Username is "$username", first name is "$firstName", last name is "$lastName", email is "$email"""")
+    val id = row["id"]
+    val userUpdateUrl = "$url/users/$id"
+    val body = JSONObject()
+    body.put("IsActive", true)
+    val (_, _, result) = userUpdateUrl.httpPost()
+        .header(Headers.AUTHORIZATION, "bearer $accessToken")
+        .jsonBody(body.toString())
+        .also { println(it) }
+        .response()
+    if (result.component2() != null) {
+        println("Update to user $id failed!")
+    }
 }
