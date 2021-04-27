892957270d70b29ced40768c76bbbb9b63ceba1a
Mike McGowan <mike.mcgowan@peoplefluent.com>
2021-04-27 11:49:05 +0100

    Initial commit

---
+++ activate-ipaas-users.main.kts
@@ -0,0 +1,38 @@
+#!/usr/bin/env kotlin
+
+// known to work with Kotlin 1.4.31
+
+@file:DependsOn("org.slf4j:slf4j-simple:1.7.30")
+@file:DependsOn("org.slf4j:slf4j-api:1.7.30")
+@file:DependsOn("io.github.microutils:kotlin-logging:1.7.9")
+@file:DependsOn("com.github.doyaaaaaken:kotlin-csv-jvm:0.15.2")
+
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.system.exitProcess
+
+if (args.isEmpty()) {
+    die("Missing input CSV file as first argument")
+}
+
+val inputFile = args[0]
+if (!Files.exists(Paths.get(inputFile))) {
+    die("""CSV file "$inputFile" does not exist""")
+}
+
+csvReader().open(inputFile) {
+    readAllAsSequence().forEach { processRow(it) }
+}
+
+fun die(reason: String) {
+    println(reason)
+    exitProcess(1)
+}
+
+/**
+ * TODO implement this function
+ */
+fun processRow(row: List<String>) {
+    println(row)
+}
